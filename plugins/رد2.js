f0252a00c716bf6af6b48cf3a88c2f3452b1a99e
Dragon <mohamedhajjej648@gmail.com>
2024-11-02 02:26:06 +0100

    Update رد2.js

--- plugins/رد2.js
+++ plugins/رد2.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = '*مــنــور يـا قــلــب فــلاش لــي الـاوامــر اكــتــب (.اوامر) ⚡️*';
-await conn.sendMessage(m.chat, { react: { text: '⚡️', key: m.key } })
+    let message = '*دراغـون تـحـت طـلـبـك 🐲 اذا احـتـجـت شـي أكـتـب (.اوامر)*';
+await conn.sendMessage(m.chat, { react: { text: '🐲', key: m.key } })
     conn.sendFile(m.chat, 'https://envs.sh/06H.jpg', 'image.jpg', message, m);
 };
 
